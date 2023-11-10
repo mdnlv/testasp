75d50ef23e303bea2f33aa9f351bdb405f5000a6
mikhail <mikdnlv@gmail.com>
2023-11-10 22:58:11 +0500

    init commit

---
+++ PassOffice/Program.cs
@@ -0,0 +1,38 @@
+﻿using PassOfficeLibrary;
+
+Console.WriteLine("БЮРО ПРОПУСКОВ");
+
+List<PassItem> initialData = new()
+{
+    new PassItem { PassNumber = 1, Name = "Данилов М. А.", PassType = 3, IssueDate = DateTime.Parse("03.11.2023") },
+    new PassItem { PassNumber = 2, Name = "Данилов Н. Н.", PassType = 2, IssueDate = DateTime.Parse("04.11.2023") },
+    new PassItem { PassNumber = 3, Name = "Данилов А. А.", PassType = 1, IssueDate = DateTime.Parse("05.11.2023") }
+};
+
+PassOffice passOffice = new(initialData);
+
+do
+{
+    Console.WriteLine("Введите действие: добавить / удалить / просмотр / поиск");
+    string? input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "добавить":
+            passOffice.Add();
+            break;
+        case "удалить":
+            passOffice.Delete();
+            break;
+        case "просмотр":
+            passOffice.View();
+            break;
+        case "поиск":
+            passOffice.Search();
+            break;
+        default:
+            Console.WriteLine("неизвестная команда");
+            break;
+    }
+    Console.WriteLine();
+} while (true);
---
+++ PassOfficeLibrary/PassOffice.cs
@@ -0,0 +1,106 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PassOfficeLibrary
+{
+    public class PassItem
+    {
+        public uint PassNumber;
+        public string Name = "";
+        public DateTime IssueDate;
+        public byte PassType;
+    }
+
+    public class PassOffice
+    {
+        internal readonly string[] _passTypeNames = { "обычный", "срочный", "транзит" };
+        
+        public List<PassItem> PassList { get; set; }
+
+        public PassOffice(List<PassItem> initialData)
+        {
+            PassList = initialData;
+        }
+    }
+
+    public static class PassOfficeExtensions
+    {
+        public static void Add(this PassOffice value)
+        {
+            Console.Write("Введите ФИО: ");
+            string newName = Console.ReadLine() ?? "";
+
+            Console.Write("Введите тип пропуска (1 - обычный, 2 - срочный, 3 - транзитный): ");
+            string newPassType = Console.ReadLine() ?? "";
+            Regex r = new Regex("[1|2|3]");
+            if (!r.IsMatch(newPassType))
+            {
+                Console.WriteLine("недопустимый тип пропуска");
+                return;
+            }
+
+            uint highestId = value.PassList.Any() ? value.PassList.Max(x => x.PassNumber) : 0;
+            value.PassList.Add(new PassItem
+            {
+                PassNumber = highestId + 1, 
+                Name = newName, 
+                PassType = Convert.ToByte(newPassType), 
+                IssueDate = DateTime.Now 
+            });
+            Console.WriteLine("пропуск добавлен");
+        }
+
+        public static void Delete(this PassOffice value)
+        {
+            try
+            {
+                Console.Write("Введите номер: ");
+                string? findNumber = Console.ReadLine();
+                int index = value.PassList.FindIndex(x => x.PassNumber == Convert.ToInt32(findNumber));
+                value.PassList.RemoveAt(index);
+                Console.WriteLine("пропуск удалён");
+            }
+            catch
+            {
+                Console.WriteLine("пропуск не найден");
+            }
+
+        }
+
+        public static void View(this PassOffice value)
+        {
+            foreach (PassItem c in value.PassList)
+            {
+                Console.WriteLine(
+                    "{0,2}\t{1,10}\t{2,10}\t{3,10}",
+                    c.PassNumber,
+                    c.Name,
+                    value._passTypeNames[c.PassType - 1],
+                    c.IssueDate.ToString("dd.MM.yyyy")
+                );
+            }
+        }
+
+        public static void Search(this PassOffice value)
+        {
+            Console.Write("Поиск с даты (дд.мм.гггг): ");
+            string startOfRange = Console.ReadLine() ?? "";
+            Console.Write("Поиск до даты (дд.мм.гггг): ");
+            string endOfRange = Console.ReadLine() ?? "";
+
+            var selectedItems = value.PassList.Where(p => 
+                p.IssueDate >= DateTime.Parse(startOfRange) && p.IssueDate <= DateTime.Parse(endOfRange)
+            );
+
+            foreach (PassItem c in selectedItems)
+            {
+                Console.WriteLine(
+                    "{0,2}\t{1,10}\t{2,10}\t{3,10}",
+                    c.PassNumber,
+                    c.Name,
+                    value._passTypeNames[c.PassType - 1],
+                    c.IssueDate.ToString("dd.MM.yyyy")
+                );
+            }
+        }
+    }
+}
